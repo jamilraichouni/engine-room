cbdec25d353c9af85f1a41cf2ecc9eb99d162e30
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-01-25 12:09:16 +0100

    chore: Move Copilot cfg to separate file

---
+++ dotfiles/nvim-config/lua/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+    -- https://github.com/zbirenbaum/copilot.lua {{{
+    {
+        "zbirenbaum/copilot.lua",
+        -- cond = function() return vim.env.NVIM == nil and vim.fn.executable("node") == 1 end,
+        cmd = { "Copilot" },
+        event = "InsertEnter",
+        opts = {
+            filetypes = {
+                -- gitcommit = true,
+                -- markdown = true,
+                -- python = true,
+                -- yaml = true,
+                ["*"] = true
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                hide_during_completion = true,
+                debounce = 75,
+                keymap = {
+                    accept = "<c-a>",
+                    accept_word = "<c-w>",
+                    accept_line = "<c-l>",
+                    next = "<c-j>",
+                    prev = "<c-k>",
+                    -- dismiss = "<C-]>",
+                },
+            },
+        },
+        -- config = function()
+        --     require("copilot").setup(opts)
+        -- end,
+    },
+}
--- dotfiles/nvim-config/lua/plugins/lsp.lua
+++ dotfiles/nvim-config/lua/plugins/lsp.lua
@@ -18,40 +18,6 @@ return {
         end
     },
     -- }}}
-    -- https://github.com/zbirenbaum/copilot.lua {{{
-    {
-        "zbirenbaum/copilot.lua",
-        -- cond = function() return vim.env.NVIM == nil and vim.fn.executable("node") == 1 end,
-        cmd = { "Copilot" },
-        event = "InsertEnter",
-        opts = {
-            filetypes = {
-                -- gitcommit = true,
-                -- markdown = true,
-                -- python = true,
-                -- yaml = true,
-                ["*"] = true
-            },
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                hide_during_completion = true,
-                debounce = 75,
-                keymap = {
-                    accept = "<c-a>",
-                    accept_word = "<c-w>",
-                    accept_line = "<c-l>",
-                    next = "<c-j>",
-                    prev = "<c-k>",
-                    -- dismiss = "<C-]>",
-                },
-            },
-        },
-        -- config = function()
-        --     require("copilot").setup(opts)
-        -- end,
-    },
-    -- }}}
     -- https://github.com/hrsh7th/nvim-cmp {{{
     {
         "hrsh7th/nvim-cmp",
