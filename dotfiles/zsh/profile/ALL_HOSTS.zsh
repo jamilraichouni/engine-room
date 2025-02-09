2bda4b9fc965b141f9d1368c2e1997fa704a797a
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-02-09 06:21:35 +0100

    fix: Remove not needed Docker setup

--- dotfiles/zsh/profile/ALL_HOSTS.zsh
+++ dotfiles/zsh/profile/ALL_HOSTS.zsh
@@ -19,8 +19,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-# # docker
-# [[ -e /var/run/docker.sock ]] && sudo chmod 777 /var/run/docker.sock
-
 # to enable work with SOPS
 gpg-agent --homedir $HOME/.gnupg --daemon 2> /dev/null
