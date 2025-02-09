9a1bd7edbe3db2e7ccb9330a68a446273a05bef4
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-02-09 06:35:55 +0100

    feat: Remove msgs/ trustfile check (uv venv on cd)

--- dotfiles/zsh/interactive/ALL_HOSTS.zsh
+++ dotfiles/zsh/interactive/ALL_HOSTS.zsh
@@ -12,28 +12,11 @@ function __python_venv_on_cd() {
   setopt local_options err_return
 
   if (($+VIRTUAL_ENV)) && [[ $PWD != ${VIRTUAL_ENV%/*}/* ]]; then
-    printf >&2 '\x1b[1m*** Leaving project venv at %q\n' $VIRTUAL_ENV
-    deactivate || unset VIRTUAL_ENV
+    deactivate > /dev/null 2>&1 || unset VIRTUAL_ENV
   fi
 
   if ! (($+VIRTUAL_ENV)) && [[ -f .venv/bin/activate ]]; then
-    if [[ -d .jj ]]; then
-      local trustfile=.jj/repo/trusted_venv
-    elif [[ -d .git ]]; then
-      local trustfile=.git/info/trusted_venv
-    else
-      local trustfile=~/.config/zsh/trusted_venvs/${PWD//\//%}
-    fi
-
-    if [[ -e $trustfile ]]; then
       . .venv/bin/activate
-      printf >&2 '\x1b[1m*** Entered project venv at %q\n' $VIRTUAL_ENV
-    else
-      printf >&2 '\x1b[1m\x1b[31m*** ERROR:\x1b[m\x1b[1m project venv is not trusted, not activating\x1b[m\n'
-      printf >&2 'To mark this venv as trusted, run:\n'
-      printf >&2 '\n'
-      printf >&2 '  touch %q\n' "$trustfile"
-    fi
   fi
 }
 
