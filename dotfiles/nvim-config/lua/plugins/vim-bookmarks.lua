f446703055993d270fa4c38aa7ce31014ec74ef6
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-02-09 06:22:38 +0100

    chore: Ensure code base w/o commented out code

--- dotfiles/nvim-config/lua/plugins/vim-bookmarks.lua
+++ dotfiles/nvim-config/lua/plugins/vim-bookmarks.lua
@@ -3,8 +3,5 @@ return {
         -- https://github.com/MattesGroeger/vim-bookmarks
         "MattesGroeger/vim-bookmarks",
         event = "VeryLazy",
-        -- config = function()
-        --     vim.keymap.nnoremap { "<leader>mp", "<Plug>BookmarkToggle" }
-        -- end,
     },
 }
