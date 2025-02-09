00b365071fbb82b3284cdfd18ccf58af30675d06
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-02-09 06:31:35 +0100

    feat: Scroll editor cursor position to top
    Applies to preview in browser.

--- dotfiles/nvim-config/lua/plugins/markdownpreview.lua
+++ dotfiles/nvim-config/lua/plugins/markdownpreview.lua
@@ -17,6 +17,20 @@ return {
             vim.g.mkdp_open_ip = "127.0.0.1"
             vim.g.mkdp_open_to_the_world = 1
             vim.g.mkdp_port = "5098"
+            vim.g.mkdp_preview_options = {
+                content_editable = false,
+                disable_filename = 0,
+                disable_sync_scroll = 0,
+                flowchart_diagrams = {},
+                hide_yaml_meta = 1,
+                katex = {},
+                maid = {},
+                mkit = {},
+                sequence_diagrams = {},
+                sync_scroll_type = "top",
+                toc = {},
+                uml = {},
+            }
             vim.g.mkdp_theme = "light"
         end
     },
