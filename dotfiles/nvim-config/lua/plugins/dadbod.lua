746f617db7221f3492fbfb1f3f009bece806148e
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-06-14 23:24:47 +0200

    feat: Add plugin to deal with databases in Neovim

---
+++ dotfiles/nvim-config/lua/plugins/dadbod.lua
@@ -0,0 +1,11 @@
+return {
+    -- https://github.com/tpope/vim-dadbod
+    {
+        "tpope/vim-dadbod",
+        cmd = { "DB" },
+    },
+    -- https://github.com/kristijanhusak/vim-dadbod-ui
+    {
+        "kristijanhusak/vim-dadbod-ui",
+    },
+}
