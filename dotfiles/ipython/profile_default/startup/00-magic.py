e7cd47292bdd64990985892ef32551a58f7acb80
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-07 01:24:35 +0200

    fix: Re-enable ipython dotenv magic for startup

--- dotfiles/ipython/profile_default/startup/00-magic.py
+++ dotfiles/ipython/profile_default/startup/00-magic.py
@@ -4,6 +4,6 @@ get_ipython().run_line_magic("load_ext", "autoreload")
 get_ipython().run_line_magic("autoreload", "2")
 try:
     get_ipython().run_line_magic("load_ext", "dotenv")
-    # get_ipython().run_line_magic("dotenv", "")
+    get_ipython().run_line_magic("dotenv", "")
 except Exception:
     print("Run 'pip install python-dotenv' to 'load_ext dotenv'!")
