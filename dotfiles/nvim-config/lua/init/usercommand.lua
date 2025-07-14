f273bc57cffa58538b66e7bde04c02a8fd076be5
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-14 23:55:02 +0200

    feat: Add user command to inspect treesitter nodes

--- dotfiles/nvim-config/lua/init/usercommand.lua
+++ dotfiles/nvim-config/lua/init/usercommand.lua
@@ -6,5 +6,6 @@ vim.api.nvim_create_user_command("Qa", "qa", { force = true })
 vim.api.nvim_create_user_command("Showautocmd", "redir! > /tmp/.autocmd | silent autocmd | redir END | e /tmp/.autocmd",
     { force = true })
 vim.api.nvim_create_user_command("Showmap", "redir! > /tmp/.map | silent map | redir END | e /tmp/.map", { force = true })
+vim.api.nvim_create_user_command("TSInspect", "Inspect!", { force = true })
 vim.api.nvim_create_user_command("WorkingTimesCompute", "lua vim.g.WorkingTimesCompute()", { force = true })
 vim.api.nvim_create_user_command("WorkingTimesSaveAndClose", "write | bdelete", { force = true })
