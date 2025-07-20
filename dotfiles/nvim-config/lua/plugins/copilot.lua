a0ab27bb606c4d885d4d02a6ff310c7c1a3ea9fc
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-20 09:44:50 +0200

    fix: Copilot configuration

--- dotfiles/nvim-config/lua/plugins/copilot.lua
+++ dotfiles/nvim-config/lua/plugins/copilot.lua
@@ -2,35 +2,52 @@ return {
     -- https://github.com/zbirenbaum/copilot.lua {{{
     {
         "zbirenbaum/copilot.lua",
-        -- cond = function() return vim.env.NVIM == nil and vim.fn.executable("node") == 1 end,
-        cmd = { "Copilot" },
+        cmd = "Copilot",
         event = "InsertEnter",
-        opts = {
-            filetypes = {
-                -- gitcommit = true,
-                -- markdown = true,
-                -- python = true,
-                -- yaml = true,
-                ["*"] = true
-            },
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                hide_during_completion = true,
-                debounce = 75,
-                keymap = {
-                    accept = "<c-a>",
-                    accept_word = "<c-w>",
-                    accept_line = "<c-l>",
-                    next = "<c-j>",
-                    prev = "<c-k>",
-                    -- dismiss = "<C-]>",
+        config = function()
+            require('copilot').setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    hide_during_completion = true,
+                    debounce = 75,
+                    trigger_on_accept = false,
+                    keymap = {
+                        accept = "<c-a>",
+                        accept_word = "<c-w>",
+                        accept_line = "<c-l>",
+                        next = "<c-j>",
+                        prev = "<c-k>",
+                        -- dismiss = "<C-]>",
+                    },
                 },
-            },
-        },
-        -- config = function()
-        --     require("copilot").setup(opts)
-        -- end,
+                filetypes = {
+                    -- gitcommit = true,
+                    -- markdown = true,
+                    -- python = true,
+                    -- yaml = true,
+                    sh = function()
+                        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+                            -- disable for .env files
+                            return false
+                        end
+                        return true
+                    end,
+                    ["*"] = true
+                },
+                should_attach = function(_, _)
+                    return true
+                end,
+                logger = {
+                    file = "/tmp/copilot-lua.log",
+                    file_log_level = vim.log.levels.OFF,
+                    print_log_level = vim.log.levels.ERROR,
+                    trace_lsp = "off", -- "off" | "messages" | "verbose"
+                    trace_lsp_progress = false,
+                    log_lsp_messages = false,
+                },
+            })
+        end,
     },
     -- }}}
     -- https://github.com/AndreM222/copilot-lualine {{{
