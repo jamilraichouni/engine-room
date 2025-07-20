de6366dbd5e6c0c15f709aaee67acbe06ab2c894
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-20 09:40:53 +0200

    feat: Add `scala` to treesitter languages

--- dotfiles/nvim-config/lua/plugins/treesitter.lua
+++ dotfiles/nvim-config/lua/plugins/treesitter.lua
@@ -48,6 +48,7 @@ return {
                     "pymanifest",
                     "python",
                     "rst",
+                    "scala",
                     "scss",
                     "toml",
                     "typescript",
