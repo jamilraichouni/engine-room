07f9ee56faff051dd9c59790c38024c263b418f5
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-20 09:43:40 +0200

    feat: Ad prefix to command
    To ease finding the command

--- dotfiles/nvim-config/lua/init/usercommand.lua
+++ dotfiles/nvim-config/lua/init/usercommand.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_user_command("HTML2FT", "%s/\\v(\\w+)\\(/\\=printf('ft.%s(',
 vim.api.nvim_create_user_command("Qa", "qa", { force = true })
 vim.api.nvim_create_user_command("Showautocmd", "redir! > /tmp/.autocmd | silent autocmd | redir END | e /tmp/.autocmd",
     { force = true })
-vim.api.nvim_create_user_command("Showmap", "redir! > /tmp/.map | silent map | redir END | e /tmp/.map", { force = true })
+vim.api.nvim_create_user_command("JarShowmap", "redir! > /tmp/.map | silent map | redir END | e /tmp/.map", { force = true })
 vim.api.nvim_create_user_command("TSInspect", "Inspect!", { force = true })
 vim.api.nvim_create_user_command("WorkingTimesCompute", "lua vim.g.WorkingTimesCompute()", { force = true })
 vim.api.nvim_create_user_command("WorkingTimesSaveAndClose", "write | bdelete", { force = true })
