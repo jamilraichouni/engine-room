5e592c2552544a5de36e6670a8c0b7cc99fea64e
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-23 14:14:12 +0200

    feat: Switch Copilot zbirenbaum -> tpope
    The version from tpope is official and maintained by GitHub.

--- dotfiles/nvim-config/lua/plugins/copilot.lua
+++ dotfiles/nvim-config/lua/plugins/copilot.lua
@@ -1,56 +1,22 @@
 return {
-    -- https://github.com/zbirenbaum/copilot.lua {{{
+    -- https://github.com/github/copilot.vim {{{
     {
-        "zbirenbaum/copilot.lua",
+        "github/copilot.vim",
         cmd = "Copilot",
+        lazy = false,
         event = "InsertEnter",
         config = function()
-            require('copilot').setup({
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    hide_during_completion = true,
-                    debounce = 75,
-                    trigger_on_accept = false,
-                    keymap = {
-                        accept = "<c-a>",
-                        accept_word = "<c-w>",
-                        accept_line = "<c-l>",
-                        next = "<c-j>",
-                        prev = "<c-k>",
-                        -- dismiss = "<C-]>",
-                    },
-                },
-                filetypes = {
-                    -- gitcommit = true,
-                    -- markdown = true,
-                    -- python = true,
-                    -- yaml = true,
-                    sh = function()
-                        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
-                            -- disable for .env files
-                            return false
-                        end
-                        return true
-                    end,
-                    ["*"] = true
-                },
-                should_attach = function(_, _)
-                    return true
-                end,
-                logger = {
-                    file = "/tmp/copilot-lua.log",
-                    file_log_level = vim.log.levels.OFF,
-                    print_log_level = vim.log.levels.ERROR,
-                    trace_lsp = "off", -- "off" | "messages" | "verbose"
-                    trace_lsp_progress = false,
-                    log_lsp_messages = false,
-                },
+            vim.keymap.set('i', '<c-a>', 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false
             })
+            vim.keymap.set('i', '<c-l>', '<Plug>(copilot-accept-line)')
+            vim.keymap.set('i', '<c-w>', '<Plug>(copilot-accept-word)')
+            vim.g.copilot_no_tab_map = true
+            vim.g.copilot_filetypes = {
+                ['*'] = true,
+            }
         end,
     },
-    -- }}}
-    -- https://github.com/AndreM222/copilot-lualine {{{
-    { 'AndreM222/copilot-lualine' }
     -- }}}
 }
