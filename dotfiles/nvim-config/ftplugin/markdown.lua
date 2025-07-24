b357cdaecb5c6febac2d08f6e4cc9973d9be7788
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-25 00:23:22 +0200

    fix: Use highlight `Title` in blue
    Make Markdown headings use the `Title` highlight group in blue.

--- dotfiles/nvim-config/ftplugin/markdown.lua
+++ dotfiles/nvim-config/ftplugin/markdown.lua
@@ -1,3 +1,2 @@
 vim.bo.textwidth = 79
 vim.wo.wrap = true
-vim.fn.matchadd("PreProc", "^#.*")
--- dotfiles/nvim-config/lua/init/highlight.lua
+++ dotfiles/nvim-config/lua/init/highlight.lua
@@ -1,4 +1,5 @@
 vim.cmd.highlight("BookmarkAnnotationSign", "guifg=#3794FF")
 vim.cmd.highlight("BookmarkSign", "guifg=#3794FF")
 -- vertical line between windows
+vim.cmd.highlight("Title", "guifg=#569cd6 guibg=#1e1e1e")
 vim.cmd.highlight("VertSplit", "guifg=#444444 guibg=#1e1e1e gui=none")
