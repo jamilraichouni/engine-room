2138765702914ddc5793049191d358d539b7932a
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-25 11:35:13 +0200

    chore: We define prompts via vim-ai-roles.ini

--- dotfiles/nvim-config/lua/plugins/chatgpt.lua
+++ dotfiles/nvim-config/lua/plugins/chatgpt.lua
@@ -23,24 +23,6 @@ return {
             vim.g.vim_ai_debug = 0
             vim.g.vim_ai_debug_log_file = "/tmp/vim_ai_debug.log"
             vim.g.vim_ai_roles_config_file = os.getenv("HOME") .. "/.config/nvim/vim-ai-roles.ini"
-            function ChatGPTEclipsePluginExpertFn()
-                local prompt = "I have a question about Eclipse plugin development."
-                local config = {
-                    engine = "chat",
-                    options = {
-                        model = "gpt-4o",
-                        initial_prompt =
-                        ">>> system\n\nYou are going to play a role of an expert in Eclipse plugin development.",
-                        max_tokens = 0,
-                        temperature = 0,
-                        request_timeout = 20,
-                        selection_boundary = '"""',
-                    },
-                }
-                vim.fn['vim_ai#AIChatRun']("%", { config = config, prompt = prompt })
-            end
-
-            vim.api.nvim_create_user_command("ChatGPTEclipsePluginExpertFn", ChatGPTEclipsePluginExpertFn, {})
         end,
     }
 }
