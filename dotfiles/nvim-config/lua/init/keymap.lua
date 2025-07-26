d3cfe3698fb53124bb1d305ed63dd7f55db2825e
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-07-26 11:06:24 +0200

    feat: Add keymap to open autocommands file

--- dotfiles/nvim-config/lua/init/keymap.lua
+++ dotfiles/nvim-config/lua/init/keymap.lua
@@ -34,6 +34,7 @@ vim.keymap.set("n", "<leader>dt", "<cmd>windo diffthis<cr>", { silent = true })
 vim.keymap.set("n", "<leader>do", "<cmd>windo diffoff<cr>", { silent = true })
 
 -- edit specific files {{{
+vim.keymap.set("n", "<leader>Auto", "<cmd>e ~/.config/nvim/lua/init/autocmd.lua<cr>", { silent = true })
 vim.keymap.set("n", "<leader>Init", "<cmd>e ~/.config/nvim/init.lua<cr>", { silent = true })
 vim.keymap.set("n", "<leader>Key", "<cmd>e ~/.config/nvim/lua/init/keymap.lua<cr>", { silent = true })
 vim.keymap.set("n", "<leader>Plug", "<cmd>Oil ~/.config/nvim/lua/plugins<cr>", { silent = true })
