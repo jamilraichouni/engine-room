955c4c53cc07a391f787e7ea197016274951f4c2
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-09-16 11:01:45 +0200

    feat: rename custom command
    `JarKeymapsAll` to show all keymaps is more accurate.

--- dotfiles/nvim-config/lua/init/usercommand.lua
+++ dotfiles/nvim-config/lua/init/usercommand.lua
@@ -5,7 +5,9 @@ vim.api.nvim_create_user_command("HTML2FT", "%s/\\v(\\w+)\\(/\\=printf('ft.%s(',
 vim.api.nvim_create_user_command("Qa", "qa", { force = true })
 vim.api.nvim_create_user_command("Showautocmd", "redir! > /tmp/.autocmd | silent autocmd | redir END | e /tmp/.autocmd",
     { force = true })
-vim.api.nvim_create_user_command("JarShowmap", "redir! > /tmp/.map | silent map | redir END | e /tmp/.map", { force = true })
+
+vim.api.nvim_create_user_command("JarKeymapsAll", "redir! > /tmp/.map | silent map | redir END | e /tmp/.map",
+    { force = true })
 vim.api.nvim_create_user_command("JarDoc",
     "edit ~/engine-room/dotfiles/nvim-config/docs/JARDOC.md",
     { force = true })
