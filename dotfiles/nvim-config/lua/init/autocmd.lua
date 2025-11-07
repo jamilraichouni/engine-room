486427bd66fb1aec43a0d38f96f87b1d8837c57d
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-11-08 00:07:25 +0100

    fix: Move the check for file at start

--- dotfiles/nvim-config/lua/init/autocmd.lua
+++ dotfiles/nvim-config/lua/init/autocmd.lua
@@ -125,12 +125,12 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     desc = "LSP format on save",
     nested = true,
     callback = function()
-        if vim.bo.filetype == "lua" or vim.bo.filetype == "markdown" or vim.bo.filetype == "python" or vim.bo.filetype == "toml" then
         -- Skip formatting for CHEATSHEET.md
         local filename = vim.fn.expand('%:t')
         if filename == "CHEATSHEET.md" then
             return
         end
+        if vim.bo.filetype == "lua" or vim.bo.filetype == "markdown" or vim.bo.filetype == "python" or vim.bo.filetype == "toml" then
             local did_something = false
 
             if next(vim.lsp.get_clients { bufnr = 0, method = "textDocument/codeAction" }) ~= nil then
