89d23371f2997966681d7ae86464001be5f0fa93
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2025-12-28 09:51:06 +0100

    perf: improve startup by disabling unused stuff

--- dotfiles/nvim-config/lua/init/global.lua
+++ dotfiles/nvim-config/lua/init/global.lua
@@ -4,3 +4,9 @@ vim.g.mapleader = " "
 vim.g.python3_host_prog = "python3"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+-- see https://github.com/neovim/neovim/tree/master/runtime/plugin
+vim.g.loaded_2html_plugin = 0
+vim.g.loaded_netrw = 0
+
