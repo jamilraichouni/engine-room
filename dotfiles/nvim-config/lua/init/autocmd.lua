885658377f0a37f334b323d1f0b9712dde0cffc7
Jamil RAICHOUNI <jamilraichouni@users.noreply.github.com>
2026-01-04 10:37:45 +0100

    feat: jump to last edit position on buffer open

--- dotfiles/nvim-config/lua/init/autocmd.lua
+++ dotfiles/nvim-config/lua/init/autocmd.lua
@@ -60,6 +60,17 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
         vim.b.copilot_enabled = false
     end
 })
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    desc = "Jump to last edit position",
+    group = vim.g.augroup_jar,
+    callback = function(arg)
+        local mark = vim.api.nvim_buf_get_mark(arg.buf, '"')
+        local lcount = vim.api.nvim_buf_line_count(arg.buf)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end
+})
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
     desc = "Colorize log files",
     group = vim.g.augroup_jar,
